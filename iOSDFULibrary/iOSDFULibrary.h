3d217c584d318049e973773d2b9e18a74d73aec0
Guido Marucci Blas <guidomb@gmail.com>
2016-12-02 15:47:02 -0300

    Add xcodeproj file for library.

---
+++ iOSDFULibrary/iOSDFULibrary.h
@@ -0,0 +1,20 @@
+//
+//  iOSDFULibrary.h
+//  iOSDFULibrary
+//
+//  Created by Guido Marucci Blas on 12/2/16.
+//  Copyright © 2016 Nordic Semiconductor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iOSDFULibrary.
+FOUNDATION_EXPORT double iOSDFULibraryVersionNumber;
+
+//! Project version string for iOSDFULibrary.
+FOUNDATION_EXPORT const unsigned char iOSDFULibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSDFULibrary/PublicHeader.h>
+
+
+#import <iOSDFULibrary/IntelHex2BinConverter.h>
