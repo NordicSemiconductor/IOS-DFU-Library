faf8bc1b6d67dd094e6e74f2fb181134034bd006
Stephen Schiffli <stephen@mbientlab.com>
2016-12-20 14:45:03 -0800

    Fix documentation warnings.

--- iOSDFULibrary/Classes/Implementation/DFUServiceInitiator.swift
+++ iOSDFULibrary/Classes/Implementation/DFUServiceInitiator.swift
@@ -175,7 +175,7 @@ import CoreBluetooth
      service (return a delegate callback if does not have such), jump to the DFU Bootloader mode
      if necessary and perform the DFU. Proper delegate methods will be called during the process.
      
-     - parameter central manager that will be used to connect to the peripheral
+     - parameter centralManager: manager that will be used to connect to the peripheral
      - parameter target: the DFU target peripheral
      
      - returns: the initiator instance
