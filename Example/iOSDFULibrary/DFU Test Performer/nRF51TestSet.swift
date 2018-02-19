b0f6d072dfb16545372bb0ec4037e06e8bd29779
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2018-02-19 11:29:30 +0100

    nRF51 tests fixed

--- Example/iOSDFULibrary/DFU Test Performer/nRF51TestSet.swift
+++ Example/iOSDFULibrary/DFU Test Performer/nRF51TestSet.swift
@@ -22,7 +22,8 @@ class nRF51TestSet: DFUTestSet {
              Option.experimentalButtonlessEnabled, nil, "Updating app from SDK 12.2",      FilterBy.name("DFU1A122")),
             // Try downgrading to SDK 8. This should fail as a) Secure DFU packet is required, b) required SD ID is invalid
             (DFUFirmware.from(zip: "nrf51422_sdk_8_app",            locatedIn:  "Firmwares/nRF51"),
-             nil, DFUError.remoteSecureDFUInvalidObject, "Updating app from SDK 8", FilterBy.name("DFU1B122")), // Name with B, as the device was switched to Bootloader mode
+             Option.experimentalButtonlessEnabled, DFUError.remoteSecureDFUInvalidObject,
+             "Updating app from SDK 8", FilterBy.name("DFU1B122")), // Name with B, as the device was switched to Bootloader mode
             // Continue with testing:
             (DFUFirmware.from(zip: "nrf51422_sdk_11_sd_bl",         locatedIn:  "Firmwares/nRF51"), nil, nil, "Downgrading to SDK 11 (SD only)", FilterBy.name("DFU1B11")),
             (DFUFirmware.from(zip: "nrf51422_sdk_11_app",           locatedIn:  "Firmwares/nRF51"), Option.prn(4), nil, "Flashing app from SDK 11 with PRN set to 4", FilterBy.name("DFU1A11")),
@@ -31,7 +32,8 @@ class nRF51TestSet: DFUTestSet {
             (DFUFirmware.from(zip: "nrf51422_sdk_8_app",            locatedIn:  "Firmwares/nRF51"), nil, nil, "Updating app from SDK 8",         FilterBy.name("DFU1A08")),
             (DFUFirmware.from(zip: "nrf51422_sdk_6.1_all_in_one",   locatedIn:  "Firmwares/nRF51"), nil, nil, "Downgrading to SDK 6.1",          FilterBy.name("DFU1A061")),
             (DFUFirmware.from(zip: "nrf51422_sdk_6.1_app",          locatedIn:  "Firmwares/nRF51"), nil, nil, "Updating app from SDK 6.1",       FilterBy.name("DFU1A061")),
-            // For the 2nd part of the following update PRNs must be enabled. Flash operation in DFU bootloader was too slow to support full upload speed.
+            // For the 2nd part of the following update PRNs will be enabled and set to 2. Additionally, a 1000 ms delay before sending the firmware is added.
+            // Flash operation in DFU bootloader was too slow to support full upload speed.
             (DFUFirmware.from(zip: "nrf51422_sdk_6.0_all_in_one",   locatedIn:  "Firmwares/nRF51"), nil, nil, "Downgrading to SDK 6",            nil)
             // App from SDK 6.0 does not have Buttonless jump service. No further update is possible.
         ]
