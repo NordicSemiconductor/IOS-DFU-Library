243f532b7e40977e96632740c7b933822389b047
Mostafa Berg <mostafa.berg@nordicsemi.no>
2018-09-27 09:47:40 +0200

    +Initial migration to Swift 4.2 and project settings update

--- Example/iOSDFULibrary/AppDelegate.swift
+++ Example/iOSDFULibrary/AppDelegate.swift
@@ -28,7 +28,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
@@ -57,4 +57,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
-
