e3bee9a5034d3c6acb7cdb98361dcc8144d4df47
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2023-11-24 23:53:27 +0100

    Renaming test app from Example to Test Performer App

--- Example/Pods/Target Support Files/Pods-NordicDFU_Example/Pods-NordicDFU_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NordicDFU_Example : NSObject
-@end
-@implementation PodsDummy_Pods_NordicDFU_Example
-@end
---
+++ Test Performer App/Pods/Target Support Files/Pods-DFU Test Performer/Pods-DFU Test Performer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DFU_Test_Performer : NSObject
+@end
+@implementation PodsDummy_Pods_DFU_Test_Performer
+@end
